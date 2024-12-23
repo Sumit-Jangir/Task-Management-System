02f285797e935139927e60c92dfee88f04e5d777
Sumit Jangir <jangirsumit1011@gmail.com>
2024-12-23 23:42:22 +0530

    Add board and table button

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,67 +1,80 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import {  useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { clearToken } from "./Redux/slice.jsx";
 
 const Header = () => {
-  const navigate = useNavigate()
-
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.token);
-
 
   const [isLogin, setIsLogin] = useState(token);
 
   const handleLogout = () => {
     dispatch(clearToken());
     setIsLogin(token);
-    navigate('/login')
+    navigate('/login');
   };
 
   useEffect(() => {
     setIsLogin(token);
-    // console.log("jjjjjjj")
-  });
+  }, [token]);
 
   return (
-    <>
-      <section
-        id="header"
-        className="bg-gray-100 shadow-md py-4 px-6 flex justify-between items-center"
-      >
-        {/* Logo/Welcome Text */}
-        <Link className="text-xl font-bold text-gray-800">
+    <section
+      id="header"
+      className="bg-[#0000003d] shadow-md py-2 px-6 flex justify-between items-center"
+    >
+      {/* Logo/Welcome Text */}
+      <div>
+        <Link className="text-2xl text-white font-bold">
           Welcome User
         </Link>
+        <NavLink
+          className={({ isActive }) =>
+            ` text-white ml-10  px-3 py-1 rounded-md ${isActive ? "bg-white text-[#172b4d]" : ""}`
+          }
+          to="/"
+        >
+          Board
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            ` text-white ml-2  px-3 py-1 rounded-md ${isActive ? "bg-white text-gray-900" : ""}`
+          }
+          to="/table"
+        >
+          Table
+        </NavLink>
+      </div>
 
-        {/* Navigation */}
-        <div className="space-x-4">
-          {isLogin ? (
-            <button
-              className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300"
-              onClick={handleLogout} // Logout button will call handleLogout
+      {/* Navigation */}
+      <div className="space-x-4">
+        {isLogin ? (
+          <button
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        ) : (
+          <>
+            <Link
+              className="px-4 py-2 bg-teal-600 text-white rounded"
+              to="/login/"
             >
-              Logout
-            </button>
-          ) : (
-            <>
-              <Link
-                className="px-4 py-2 bg-teal-600 text-white rounded"
-                to={"/login/"}
-              >
-                Login
-              </Link>
-              <Link
-                className="px-4 py-2 bg-teal-600 text-white rounded"
-                to={"/signup/"}
-              >
-                SignUp
-              </Link>
-            </>
-          )}
-        </div>
-      </section>
-    </>
+              Login
+            </Link>
+            <Link
+              className="px-4 py-2 bg-teal-600 text-white rounded"
+              to="/signup/"
+            >
+              SignUp
+            </Link>
+          </>
+        )}
+      </div>
+    </section>
   );
 };
 
