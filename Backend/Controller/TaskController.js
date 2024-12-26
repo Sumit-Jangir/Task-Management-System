ad5f5ab8d86bac804a51b2891df7aafb97788185
Sumit Jangir <jangirsumit1011@gmail.com>
2024-12-26 21:28:05 +0530

    create dueDate api

--- Backend/Controller/TaskController.js
+++ Backend/Controller/TaskController.js
@@ -1,88 +1,105 @@
-import mongoose from 'mongoose';  
-import taskSchema from '../Model/taskSchema.js';
-import listSchema from '../Model/listSchema.js';  
+import mongoose from "mongoose";
+import taskSchema from "../Model/taskSchema.js";
+import listSchema from "../Model/listSchema.js";
 
 export const createTask = async (req, res) => {
-    const { name, listId,userId ,listName} = req.body;
-    try {
-        const list = await listSchema.findById(listId);
-        if (!list) {
-            return res.status(404).json({ error: "List not found" });
-        }
-
-        const task = await taskSchema.create({ name, listId, userId ,listName});
-
-        res.status(200).json(task);  
-    } catch (error) {
-        console.error("Error creating task:", error);
-        res.status(500).json({ error: error.message });
+  const { name, listId, userId, listName } = req.body;
+  try {
+    const list = await listSchema.findById(listId);
+    if (!list) {
+      return res.status(404).json({ error: "List not found" });
     }
-}; 
 
-export const addDueDate = async (req, res) => {
-    const { taskId, dueDate } = req.body; 
-    try {
-        const task = await taskSchema.findByIdAndUpdate(
-            taskId, 
-            { dueDate: dueDate }, 
-            // { new: true }  
-        );
+    const task = await taskSchema.create({ name, listId, userId, listName });
 
-        if (!task) {
-            return res.status(404).json({ error: "Task not found" });
-        }
-
-        res.status(200).json(task);  
-    } catch (error) {
-        console.error("Error updating task:", error);
-        res.status(500).json({ error: error.message });
-    }
+    res.status(200).json(task);
+  } catch (error) {
+    console.error("Error creating task:", error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
+export const addDueDate = async (req, res) => {
+  try {
+    const { taskId, dueDate } = req.body;
+    const task = await taskSchema.findByIdAndUpdate(
+      taskId,
+      { dueDate: dueDate }
+      // { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.error("Error updating task:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getTasksWithDueDate = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      return res.status(400).json({ error: "User ID is required" });
+    }
+
+    const tasks = await taskSchema.find({
+      userId,
+      dueDate: { $exists: true, $ne: null },
+    });
+
+    return res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const getTasksForList = async (req, res) => {
-    const { listId } = req.params;
-    try {
-        if (!mongoose.Types.ObjectId.isValid(listId)) {
-            return res.status(400).json({ error: "Invalid listId" });
-        }
-
-        const tasks = await taskSchema.find({ listId });
-
-        res.status(200).json(tasks);
-    } catch (error) {
-        console.error("Error fetching tasks:", error);
-        res.status(500).json({ error: "Failed to fetch tasks." });
+  const { listId } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(listId)) {
+      return res.status(400).json({ error: "Invalid listId" });
     }
+
+    const tasks = await taskSchema.find({ listId });
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    res.status(500).json({ error: "Failed to fetch tasks." });
+  }
 };
 
 export const getAllTasks = async (req, res) => {
-    try {
-        const { userId } = req.body;
+  try {
+    const { userId } = req.body;
 
-        const tasks = await taskSchema.find({userId});
+    const tasks = await taskSchema.find({ userId });
 
-        res.status(200).json(tasks);
-    } catch (error) {
-        console.error("Error fetching tasks:", error);
-        res.status(500).json({ error: "Failed to fetch tasks." });
-    }
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    res.status(500).json({ error: "Failed to fetch tasks." });
+  }
 };
 
 export const updateTask = async (req, res) => {
-    const { taskId } = req.params;
-    const { listId } = req.body;
-  
-    console.log("jh");
-    try {
-        
-      const task = await taskSchema.findById(taskId);
-      task.listId = listId; 
-      await task.save();
-  
-      res.status(200).json(task);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
-  
+  const { taskId } = req.params;
+  const { listId } = req.body;
+
+  console.log("jh");
+  try {
+    const task = await taskSchema.findById(taskId);
+    task.listId = listId;
+    await task.save();
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
