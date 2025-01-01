870e6725f5f7979953473a7aadd450f41c54f164
Sumit Jangir <jangirsumit1011@gmail.com>
2025-01-01 23:53:51 +0530

    send referral code using nodemailer

--- Backend/Router/settingRoute.js
+++ Backend/Router/settingRoute.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { createAddUpdateNavList, getNavList } from '../Controller/settingController.js';
+import { createAddUpdateNavList, getBgUrl, getNavList, getReferredUsers, sendReferral, setBgUrl } from '../Controller/settingController.js';
 
 const route = express.Router();
 
 route.post('/createAndUpdate', createAddUpdateNavList )
 route.post('/getNavList', getNavList )
+route.post('/setUrl',setBgUrl)
+route.post('/getUrl',getBgUrl)
+route.post('/sendReferral',sendReferral)
+route.post('/getReferredUsers',getReferredUsers)
 
 export default route;   
