02f2b3515a22ba35828c7fa18b5d0506e5dca567
Sumit Jangir <jangirsumit1011@gmail.com>
2025-01-25 00:50:56 +0530

    update calendar

--- Frontend/src/Components/Calendear/Calendar.jsx
+++ Frontend/src/Components/Calendear/Calendar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './calendar.css'; 
+import './Calendar.css'; 
 import axios from 'axios';
 
 const localizer = momentLocalizer(moment);
